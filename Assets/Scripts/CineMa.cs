82d9b66dabbf8ed5da0ad63a8bbd196c9367d01a
akasharkxx <akash.jaiswar41@gmail.com>
2020-06-18 12:10:51 +0530

    Added new levels
    Added new level
    Added more depth to level 1
    This repo will be dead for sometime

---
+++ Assets/Scripts/CineMa.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CineMa : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    public float cameraSmoothness = .5f; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.x > transform.position.x)
+        {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }    
+    }
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rocketRigidBody;
+    AudioSource thrustAudio;
+
+    [SerializeField]
+    float rcsThrust = 100f;
+    [SerializeField]
+    float mainThurst = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocketRigidBody = GetComponent<Rigidbody>();
+        thrustAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Thrust();
+        Rotation();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("dead");
+                break;
+        }
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            float thrustThisFrame = mainThurst * Time.deltaTime;
+            //Thrust Rocket
+            rocketRigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+            if (!thrustAudio.isPlaying)
+                thrustAudio.Play();
+        }
+        else
+        {
+            thrustAudio.Stop();
+        }
+    }
+    private void Rotation()
+    {
+        rocketRigidBody.freezeRotation = true;
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Rotate Left
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Rotate Right
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rocketRigidBody.freezeRotation = false;
+    }
+}
